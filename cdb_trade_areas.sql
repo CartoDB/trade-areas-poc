d3d0c941e9f34702e3f67b01135d6c7d8aae6a59
Rafa de la Torre <rtorre@cartodb.com>
2016-01-20 19:49:07 +0100

    Small re-arrangement of the code

--- cdb_trade_areas.sql
+++ cdb_trade_areas.sql
@@ -1,25 +1,23 @@
--- Poc for the Routing API focused on trade areas
+-- Poc for the Routing API focused on trade areas.
+--
 -- This code will have to be suffled around in order to:
 --   * split server and client extension code
 --   * isolate configs
 --   * add quota checks and usage tracking
 
--- Requirements:
+-- * Requirements:
 --  * Add your app_id and and app_code to cdb_conf table:
 --      SELECT cdb_configure_heremaps('your_app_id', 'your_app_code');
 --
--- TODO: implement sample function
--- TODO: implement example of usage
-
--- Type used for return value
-CREATE TYPE isoline AS (
-    center geometry(Geometry,4326),
-    range integer,
-    the_geom geometry(Geometry,4326)
-);
+--  * Package and install the python library:
+--      ```
+--      cd lib/python/cdb
+--      python setup.py sdist
+--      pip install ./dist/cdb-0.0.1.tar.gz
+--      ```
 
 
--- Small helper to configure locally
+-- Helper to configure locally
 CREATE OR REPLACE FUNCTION cdb_configure_heremaps(app_id text, app_code text)
 RETURNS void AS $$
 DECLARE
@@ -31,13 +29,33 @@ END
 $$ LANGUAGE plpgsql;
 
 
+
+-- Type used for return value
+CREATE TYPE isoline AS (
+    center geometry(Geometry,4326),
+    range integer,
+    the_geom geometry(Geometry,4326)
+);
+
+
+
+-- The important function that should drive the API
 CREATE OR REPLACE FUNCTION cdb_isodistance(
     start geometry(Geometry,4326),
     destination geometry(Geometry,4326),
     mode text,
     distance integer[]
 ) RETURNS SETOF isoline AS $$
-  return []
+    import cdb.here.routing
+
+    #--TODO: get config
+
+    #--TODO: move instantiation somewhere else
+
+    #--client = cdb.here.routing.Client()
+
+    #--mocked response
+    return []
 $$ LANGUAGE plpythonu;
 
 
