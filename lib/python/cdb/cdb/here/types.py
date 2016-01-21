3200e40a05fbad8479dba4a4711f776edf0ea76a
Rafa de la Torre <rtorre@cartodb.com>
2016-01-21 18:14:55 +0100

    Implement type conversion

--- lib/python/cdb/cdb/here/types.py
+++ lib/python/cdb/cdb/here/types.py
@@ -4,7 +4,13 @@ import plpy
 
 # Convert a HERE polyline shape to a PostGIS multipolygon
 def geo_polyline_to_multipolygon(polyline):
-    #-- mocked response
-    return plpy.execute("SELECT ST_MPolyFromText('MULTIPOLYGON(((0 0 1,20 0 1,20 20 1,0 20 1,0 0 1),(5 5 3,5 7 3,7 7 3,7 5 3,5 5 3)))', 4326) as geom", 1)[0]['geom']
+    coordinates = []
+    for point in polyline:
+        lat, lon = point.split(',')
+        coordinates.append("%s %s" % (lon, lat))
+    wkt_coordinates = ','.join(coordinates)
 
+    plpy.notice(wkt_coordinates)
 
+    geometry = plpy.execute("SELECT ST_MPolyFromText('MULTIPOLYGON(((%s)))', 4326) as geom" % wkt_coordinates, 1)[0]['geom']
+    return geometry
